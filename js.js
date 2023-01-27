cf779792e9c3aecce32b7a50817b6b4eaaa9b983
achouri mohsen <achorimohsen@gmail.com>
2023-01-27 17:55:43 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+let h5=document.getElementById('html');
+
+function hcj(news){
+    h5.src=news
+}
+let section=document.querySelector('section');
+
+function khalfya(kk){
+    section.style.background=kk;
+}
